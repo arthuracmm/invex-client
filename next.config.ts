4fc79bae6a808c95fe739fcab8a577ecd3d13dfe
hugozera1 <hugorodrigues1052@gmail.com>
2026-01-02 17:44:11 -0300

    adicionando resiarte

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig = {
       'https://hugozera.space',
       'https://www.hugozera.space',
       'https://api.hugozera.space',
+      'https://resiarteakin.com.br',
+      'https://www.resiarteakin.com.br',
+      'https://api.resiarteakin.com.br',
       'http://localhost:3001'
     ]
   }
