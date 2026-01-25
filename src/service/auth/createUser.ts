d59c1b36434decef8a4eab6ebcf4046a463b8593
hugozera1 <hugorodrigues1052@gmail.com>
2026-01-25 17:41:32 -0300

    feat: Implement user creation with registration code validation, configuring `NEXT_PUBLIC_CODE` in the Dockerfile.

--- src/service/auth/createUser.ts
+++ src/service/auth/createUser.ts
@@ -3,9 +3,9 @@ import api from "./api";
 export const UsersApi = {
     create: async (body: any, code: string) => {
         if (process.env.NEXT_PUBLIC_CODE !== code) {
-            console.log("Código de registro inválido");
-            return;
+            console.log("Código de registro inválido"); return;
         }
+
         try {
             const response = await api.post("/users", {
                 ...body,
